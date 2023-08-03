fcc2b1c1538c07cd5bb9141705ae4062a8178623
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-08-03 20:52:03 +0600

    Sidebar complete

--- src/Dashboard/DashboardComponents/Sidebar.jsx
+++ src/Dashboard/DashboardComponents/Sidebar.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import logo from "../../assets/lumenlogo/lumenlogo/lumensofttech-01.svg";
 import { useState } from "react";
 import { AiOutlineDashboard, AiFillAppstore } from "react-icons/ai";
+import { RiPagesLine } from "react-icons/ri";
+import { FaLayerGroup, FaUsers } from "react-icons/fa";
+import { FcGenealogy } from "react-icons/fc";
+import { HiOutlineMail } from "react-icons/hi";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
 import DropDownMenu from "./DropDown/DropDownMenu";
 
 const Sidebar = () => {
@@ -23,6 +28,47 @@ const Sidebar = () => {
             icon={<AiFillAppstore />}
             lebel={"App"}
             menu={["Not Set yet"]}
+          />
+          <li className='text-cyan-400 font-bold'>Pages</li>
+          <DropDownMenu
+            icon={<RiPagesLine />}
+            lebel={"All Pages "}
+            menu={["About Us", "Blog", "Services", "Landing Page"]}
+          />
+          <DropDownMenu
+            icon={<FaLayerGroup />}
+            lebel={"Layout"}
+            menu={["Not Set yet"]}
+          />
+          <li className='text-cyan-400 font-bold'>Components </li>
+          <DropDownMenu
+            icon={<FcGenealogy />}
+            lebel={"UI Elements"}
+            menu={[
+              "buttons",
+              "Alerts",
+              "Progress Bar",
+              "Number Counter ",
+              "Service Control ",
+              "Home carousel",
+              " Testimonials",
+              " Dropdown Menu ",
+            ]}
+          />
+          <DropDownMenu
+            icon={<LiaFileInvoiceDollarSolid />}
+            lebel={"Invoices"}
+            menu={["Invoices List "]}
+          />
+          <DropDownMenu
+            icon={<HiOutlineMail />}
+            lebel={"Email"}
+            menu={["Inbox", "Templates"]}
+          />
+          <DropDownMenu
+            icon={<FaUsers />}
+            lebel={"Team Managers"}
+            menu={["Team Create ", "Team Details "]}
           />
         </ul>
       </div>
