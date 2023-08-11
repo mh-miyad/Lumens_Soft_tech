0ad9d6bdd6846c2db34b6284433d2b0cae63a610
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-08-11 22:08:49 +0600

    add

--- src/Components/Scroll/Scroll.jsx
+++ src/Components/Scroll/Scroll.jsx
@@ -26,6 +26,25 @@ const Scroll = () => {
         skypeScrollUp.classList.remove("active");
       }
     });
+    // const chatbox = document.getElementById("fb-customer-chat");
+    // chatbox.setAttribute("page_id", "105492139219124");
+    // chatbox.setAttribute("attribution", "biz_inbox");
+    // window.fbAsyncInit = function () {
+    //   FB.init({
+    //     xfbml: true,
+    //     version: "v17.0",
+    //   });
+    // };
+
+    // (function (d, s, id) {
+    //   var js,
+    //     fjs = d.getElementsByTagName(s)[0];
+    //   if (d.getElementById(id)) return;
+    //   js = d.createElement(s);
+    //   js.id = id;
+    //   js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    //   fjs.parentNode.insertBefore(js, fjs);
+    // })(document, "script", "facebook-jssdk");
   }, []);
   return (
     <div>
@@ -34,6 +53,12 @@ const Scroll = () => {
           <i className='fa-solid fa-angles-up'></i>
         </a>
       </div>
+
+      {/* <div id='fb-root'></div>
+
+      {/* <!-- Your Chat Plugin code --> */}
+      {/* <div id='fb-customer-chat' class='fb-customerchat'></div>  */}
+
       {/* <!-- scroll to top button -->
 
     <!-- whatsapp to top button --> */}
